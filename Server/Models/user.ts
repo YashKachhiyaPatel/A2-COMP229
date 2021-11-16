3434d9fb741cf24666aef805e1da31383d505209
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-11-15 20:49:18 -0500

    templete

--- Server/Models/user.ts
+++ Server/Models/user.ts
@@ -7,6 +7,10 @@ const UserSchema = new Schema
     username: String,
     emailAddress: String,
     displayName: String,
+    isowner: {
+        type: Boolean,
+        default: true
+    },
     created: 
     {
         type: Date,
